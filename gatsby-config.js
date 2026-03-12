4f1a294c164ec001704622b0a213e211787023bc
fukumoto-horse <fukumoto.nss@gmail.com>
2026-03-12 18:42:51 +0900

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,12 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#fff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon/favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
